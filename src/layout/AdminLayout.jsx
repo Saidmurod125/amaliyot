4fcf51b9e94a78eb12e3f587572ead58f417af1b
Ruslan <ruslantilovov2@gmail.com>
2024-05-13 14:03:10 +0300

    add some functions

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,18 +1,27 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
 import AdminHeader from "../pages/Admin/anjumanlar/AdminHeader";
 import AdminAside from "../pages/Admin/anjumanlar/AdminAside";
+import Loader from "../components/Loader";
+import useAxios from "../service/useAxios";
 
 const AdminLayout = () => {
+  const { loading } = useAxios(
+    "https://6634d6199bb0df2359a2e7ca.mockapi.io/conference/conference"
+  );
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <AdminHeader />
-      <div className="flex ">
-      <AdminAside />
-      <Outlet />
-
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+          <AdminHeader />
+          <div className="flex ">
+            <AdminAside />
+            <Outlet />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
